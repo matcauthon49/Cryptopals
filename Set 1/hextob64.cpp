850485304fb868e39fe3a923b9299927cce2f8d2
Naman Kumar <96226185+matcauthon49@users.noreply.github.com>
2022-03-09 18:26:38 +0530

    Better formatting of Challenge 1

--- Set 1/hextob64.cpp
+++ Set 1/hextob64.cpp
@@ -14,57 +14,78 @@ const std::string base64_ref = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 // ------------------ Decode Hex -----------------------
 
 std::vector<unsigned char> hex_to_byte(std::string hex_string) {
+  
     std::vector<unsigned char> bytearray;
+  
     for (int i = 0; i < hex_string.length(); i++) {
         int loc = hex_ref.find(hex_string[i]);
         bytearray.push_back(loc);
     }
+  
     return bytearray;
 }
 
 // ------------------ Convert Decoded Hex to Base64 -----------------------
 
 std::vector<unsigned char> hex_to_base64(std::vector<unsigned char> hex_bytearray) {
+  
     std::vector<unsigned char> binary_array;
     std::vector<unsigned char> base64_bytearray;
+  
     for (int i = 0; i < hex_bytearray.size(); i++) {
         binary_array.push_back(hex_bytearray[i]/4);
         binary_array.push_back(hex_bytearray[i]%4);
     }
+  
     int offset = binary_array.size()%3;
+  
     // std::cout << offset << "\n";
+  
     if (offset == 0) {
+      
         for (int  i = 0; i < binary_array.size(); i = i + 3){
             unsigned char b64_byte = binary_array[i]*16 + binary_array[i+1]*4 + binary_array[i+2];
             base64_bytearray.push_back(b64_byte);
         }
     }
+  
     else if (offset == 1) {
+      
         base64_bytearray.push_back(binary_array[0]);
+      
         for (int  i = 1; i < binary_array.size(); i = i + 3){
             unsigned char b64_byte = binary_array[i]*16 + binary_array[i+1]*4 + binary_array[i+2];
             base64_bytearray.push_back(b64_byte);
         }
     }
+  
     else if (offset == 2) {
+      
         base64_bytearray.push_back(binary_array[0]*4 + binary_array[1]);
+      
         for (int  i = 2; i < binary_array.size(); i = i + 3){
             unsigned char b64_byte = binary_array[i]*16 + binary_array[i+1]*4 + binary_array[i+2];
             base64_bytearray.push_back(b64_byte);
         }
     }
+  
     return base64_bytearray;
 }
 
 // ------------------ Encode Base64 -----------------------
 
 std::string byte_to_base64(std::vector<unsigned char> bytearray) {
+  
     std::string base64_string = "";
+  
     for (int i = 0; i < bytearray.size(); i++) {
+      
         unsigned char x = bytearray[i];
         unsigned char y = base64_ref[x];
+      
         base64_string.push_back(y);
     }
+  
     return base64_string;
 }
 
